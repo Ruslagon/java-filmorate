67fa42cdd16571ab5cac829f4bbfc6314725623e
Ruslagon <ruslan003.g@yandex.ru>
2023-07-28 02:27:00 +0300

    feat:вернуть тесты

--- src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
@@ -3,11 +3,16 @@ package ru.yandex.practicum.filmorate.storage;
 import java.util.List;
 
 public interface Storage<Item> {
+
     Item add(Item item);
+
     Item delete(Item item);
+
     Item update(Item item);
+
     boolean contains(Long id);
 
     Item getItem(Long id);
+
     List<Item> getAllItemsList();
 }

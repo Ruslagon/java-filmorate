8f617f475abcbca41b751f8b3832ff60e7e327e2
Ruslagon <ruslan003.g@yandex.ru>
2023-07-30 00:05:47 +0300

    refactor: перевести валидацию в форму декларирования

--- src/main/java/ru/yandex/practicum/filmorate/validator/FirstFilmDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/FirstFilmDate.java
@@ -6,11 +6,10 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = FirstFilmDateConstraintValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FirstFilmDate {
     String message() default "{дата фильма должна быть не раньше 28 декабря 1895 года}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
-
 }

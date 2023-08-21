d393dcb3993f5f84e09878c7a7129fc2adafd2c9
Ruslagon <ruslan003.g@yandex.ru>
2023-08-21 04:51:44 +0300

    refactor: переработать хранение информации о жанре и рейтинге фильма

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,10 +1,44 @@
 package ru.yandex.practicum.filmorate.model;
 
-public enum Genre {
-    COMEDY,
-    DRAMA,
-    CARTOON,
-    THRILLER,
-    DOCUMENTARY,
-    ACTION
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Genre {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+        switch (id) {
+            case (1):
+                name = "Комедия";
+                break;
+            case (2):
+                name = "Драма";
+                break;
+            case (3):
+                name = "Мультфильм";
+                break;
+            case (4):
+                name = "Триллер";
+                break;
+            case (5):
+                name = "Документальный";
+                break;
+            default:
+                name = "Боевик";
+                break;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
 }
